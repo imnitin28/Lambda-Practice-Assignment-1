57343b6da64510107e7a0bb7aea5aa3ecf7935b7
Nitin Mishra <nitin.mishra@knoldus.com>
2021-03-06 10:39:55 +0530

    Updating unit test code

--- src/test/java/com/knoldus/unit/ReturnMaximumOfTwoNumbersImplTest.java
+++ src/test/java/com/knoldus/unit/ReturnMaximumOfTwoNumbersImplTest.java
@@ -10,19 +10,19 @@ public class ReturnMaximumOfTwoNumbersImplTest {
     {
         ReturnMaximumOfTwoNumbersImpl returnMaximumOfTwoNumbers = new ReturnMaximumOfTwoNumbersImpl();
         int maximumTest1 = returnMaximumOfTwoNumbers.getMaximumOfTwoNumbers(0,0);
-        Assert.assertEquals(0,0);
+        Assert.assertEquals(0,maximumTest1);
 
         int maximumTest2 = returnMaximumOfTwoNumbers.getMaximumOfTwoNumbers(0,100);
-        Assert.assertEquals(100,100);
+        Assert.assertEquals(100,maximumTest2);
 
         int maximumTest3 = returnMaximumOfTwoNumbers.getMaximumOfTwoNumbers(-2147483648,2147483647);
-        Assert.assertEquals(2147483647,2147483647);
+        Assert.assertEquals(2147483647,maximumTest3);
 
         int maximumTest4 = returnMaximumOfTwoNumbers.getMaximumOfTwoNumbers(9999,1000);
-        Assert.assertEquals(9999,9999);
+        Assert.assertEquals(9999,maximumTest4);
 
         int maximumTest5 = returnMaximumOfTwoNumbers.getMaximumOfTwoNumbers(10000000,10000000);
-        Assert.assertEquals(10000000,10000000);
+        Assert.assertEquals(10000000,maximumTest5);
     }
 
 }
